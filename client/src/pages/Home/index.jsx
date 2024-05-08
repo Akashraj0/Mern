5a8aee90cbab413fced2e35eada59cfdcc029fd2
akashperumalraj0@gmail.com <akashperumalraj0@gmail.com>
2024-05-08 17:27:05 +0530

    first commit

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,31 @@
 import { Link } from "react-router-dom";
+// import Logo from "../../assets/Logo.jpg";
+import "./index.scss";
 const Home = () => {
   return (
     <div className="Home">
-      <Link to="/post">Post</Link>
-      <Link to="/ques">Question</Link>
+      <nav>
+        <div className="logo">
+          {/* <img src={Logo} style={{ width: "5%" }} /> */}
+        </div>
+        <div className="link">
+          <ul>
+            <li>Post</li>
+            <li>Questions</li>
+          </ul>
+        </div>
+        <div className="logout">
+          <p>LogOut</p>
+        </div>
+      </nav>
+      <div className="Container">
+        <div className="box">
+          <Link to="/post">Post</Link>
+        </div>
+        <div className="box">
+          <Link to="/ques">Question</Link>
+        </div>
+      </div>
     </div>
   );
 };
